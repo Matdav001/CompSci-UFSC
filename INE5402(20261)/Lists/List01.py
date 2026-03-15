bb24e0c3650c02873af664d38d60e97fe2069445
Matdav001 <105984884+Matdav001@users.noreply.github.com>
2026-03-15 17:28:01 -0300

    feat(INE5402): First Structural Commit.
    * First commit with the first list.

---
+++ INE5402(20261)/Lists/List01.py
@@ -0,0 +1,151 @@
+#
+# Excercise 1003
+#
+""" B = int(input())
+A = int(input())
+
+SOMA = A + B
+print("SOMA =", SOMA) """
+
+#
+# Excercise 1004
+#
+""" A = int(input())
+B = int(input())
+
+PROD = A * B
+print("PROD =", PROD) """
+
+#
+# Excercise 1005
+#
+""" A = float(input())
+B = float(input())
+
+MEDIA = ((A * 3.5) + (B * 7.5)) / 11
+print('MEDIA = %.5f' % MEDIA) """
+
+#
+# Excercise 1006
+#
+""" A = float(input())
+B = float(input())
+C = float(input())
+
+MEDIA = ((A * 2) + (B * 3) + (C * 5)) / 10
+print('MEDIA = %.1f' % MEDIA) """
+
+#
+# Excercise 1007
+#
+""" A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+DIFERENCA =  (A * B - C * D)
+print('DIFERENCA =', DIFERENCA) """
+
+#
+# Excercise 1008
+#
+""" NUM = int(input())
+HOURS = int(input())
+VALUE = float(input())
+
+SALARIO = HOURS * VALUE
+print('NUMBER =', NUM)
+print('SALARY = U$ %.2f' % SALARIO) """
+
+#
+# Excercise 2374
+#
+""" N = int(input())
+M = int(input())
+
+PRESSUREDIFF = N - M
+print(PRESSUREDIFF) """
+
+#
+# Excercise 2413
+#
+""" T = int(input())
+
+print(T*4) """
+
+#
+# Excercise 1012
+#
+""" A = float(input())
+B = float(input())
+C = float(input())
+
+TRIANGULO = (A * C) / 2
+CIRCULO = 3.14159 * (C ** 2)
+TRAPEZIO = ((A + B) * C) / 2
+QUADRADO = B ** 2
+RETANGULO = A * B
+
+print('TRIANGULO = U$ %.3f' % TRIANGULO)
+print('CIRCULO = U$ %.3f' % CIRCULO)
+print('TRAPEZIO = U$ %.3f' % TRAPEZIO)
+print('QUADRADO = U$ %.3f' % QUADRADO)
+print('RETANGULO = U$ %.3f' % RETANGULO) """
+
+#
+# Excercise 1020
+#
+""" A = int(input())
+
+ANO = int(A / 365)
+MES = int(A % 365 / 30)
+DIAS = A % 365 % 30
+print(ANO, 'ano(s)')
+print(MES, 'mes(es)')
+print(DIAS, 'dia(s)') """
+
+#
+# Excercise 1015
+#
+""" X1 = float(input())
+Y1 = float(input())
+X2 = float(input())
+Y2 = float(input())
+
+DIST = (((X2-X1)**2)+((Y2-Y1)**2))**0.5
+print('%.4f' % DIST) """
+
+#
+# Excercise 1019
+#
+""" N = int(input())
+
+HORA = int(N / 3600)
+MIN = int(N % 3600 / 60 )
+SEG = N % 60 % 60
+print('%d:%d:%d' % (HORA, MIN, SEG)) """
+
+#
+# Excercise 1017
+#
+""" T = int(input())
+V = int(input())
+
+print('%.3f' % ((T*V)/12)) """
+
+#
+# Excercise 1014
+#
+""" X = int(input())
+Y = float(input())
+
+print('%.3f km/l' % (X/Y)) """
+
+#
+# Excercise 1009
+#
+""" NAME = input()
+SALARY = float(input())
+SALES = float(input())
+
+print('TOTAL = R$ %.2f' % (SALARY + (SALES*0.15))) """
